fe34d73d0294afbd3a27e0917da0f7a25d5c8e7b
unknown <supradi765@gmail.com>
2025-05-23 14:49:56 +0800

    add migration all table

---
+++ app/Http/Controllers/LoanBookController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LoanBook;
+use Illuminate\Http\Request;
+
+class LoanBookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LoanBook $loanBook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LoanBook $loanBook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LoanBook $loanBook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LoanBook $loanBook)
+    {
+        //
+    }
+}
